38c29e35bea4cc38b60f4b18df9c6dc6a32923c0
mandigonzalez <gonzalam@umich.edu>
2014-11-21 14:33:38 -0500

    Added H1 tag

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Los Graduados - Latino Graduate Student Organization at the University of Michigan</title>
+	<title> Los Graduados - Latino Graduate Student Organization at the University of Michigan </title>
 </head>
 <body>
-	
+	<h1> Welcome! </h1>
 </body>
 </html>
