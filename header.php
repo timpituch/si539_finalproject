a88c72edb86a527d3c28f9b95aa2471d16b0e1fc
timpituch <tpituch@umich.edu>
2014-11-21 14:44:44 -0500

    Navbar changes

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
 <nav class="navbar navbar-default navbar-static-top" role="navigation"> 
 <div class="container">	
 		 <div class="navbar-header">
-			<a href="index.php" class="navbar-brand"> Mandi Gonzalez </a>
+			<a href="index.php" class="navbar-brand"> Los Graduados </a>
 			<button class="navbar-toggle" data-toggle = "collapse" data-target = ".navHeaderCollapse" value="menu_button">
 				<span class="bruh">	Menu Button </span>
 				<span class="icon-bar"> </span> 
@@ -18,23 +18,23 @@
 				echo "class = \"active\"";
 			} ?>><a href="index.php"> HOME </a></li>
 			<li <?php 
-				if (basename($_SERVER["REQUEST_URI"]) == "portfolio.php")
-					{echo "class = \"active\"";}
-				else if (basename($_SERVER["REQUEST_URI"]) == "lifetimelibrary.php")
-					{echo "class = \"active\"";}
-				else if (basename($_SERVER["REQUEST_URI"]) == "design_jams.php")
-					{echo "class = \"active\"";}
-				else if (basename($_SERVER["REQUEST_URI"]) == "chi2015.php")
-					{echo "class = \"active\"";}
-				?>><a href="portfolio.php"> PORTFOLIO </a></li>
+				if (basename($_SERVER["REQUEST_URI"]) == "about.php")
+					{echo "class = \"active\"";
+			}
+				?>><a href="about.php"> ABOUT </a></li>
 			<li <?php 
-				if (basename($_SERVER["REQUEST_URI"]) == "about.php"){
-				echo "class = \"active\"";
-			} ?>><a href="about.php"> ABOUT </a></li>
+				if (basename($_SERVER["REQUEST_URI"]) == "event.php")
+					{echo "class = \"active\"";
+			}
+				?>><a href="event.php"> EVENTS </a></li>
 			<li <?php 
-				if (basename($_SERVER["REQUEST_URI"]) == "resume.php"){
+				if (basename($_SERVER["REQUEST_URI"]) == "register.php"){
 				echo "class = \"active\"";
-			} ?>><a href="resume.php"> RESUME </a></li>
+			} ?>><a href="register.php"> REGISTER </a></li>
+			<li <?php 
+				if (basename($_SERVER["REQUEST_URI"]) == "contact.php"){
+				echo "class = \"active\"";
+			} ?>><a href="contact.php"> CONTACT </a></li>
 		  </ul>
 		 </div>	
 </div>		
--- index.php
+++ index.php
@@ -3,9 +3,10 @@
 <head>
 	<meta charset="UTF-8">
 	<title> Los Graduados - Latino Graduate Student Organization at the University of Michigan </title>
+	<link href="bootstrap.css" rel="stylesheet" type="text/css">
+	<link href="style.css" rel="stylesheet" type="text/css">
 </head>
 <body>
-
 	<?php $fname = basename(__FILE__); ?> 
 	<?php include("header.php"); ?>
 	
