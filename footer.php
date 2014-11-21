7f45f5f996e7869c166fb1d1fd8ff22b8524d68b
timpituch <tpituch@umich.edu>
2014-11-21 16:40:42 -0500

    Changed footer slightly

--- footer.php
+++ footer.php
@@ -1,8 +1,8 @@
 <footer class="footer">
 	<div class="container">
 		<p>This page was created by Mandi Gonzalez, Tim Pituch &amp; Darryl Cooper &copy; 2014 <br>
-		<em>Last modified: <?php date_default_timezone_set('America/Toronto');
-		echo date("F d, Y g:i a.", filemtime($fname)) ?> </em>
+		Last modified <?php date_default_timezone_set('America/Toronto');
+		echo date("F d, Y g:i a", filemtime($fname)) ?>
 		</p>
 	</div>
 </footer>
