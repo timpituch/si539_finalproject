b5b29ad5de4913c3f8b4b7ce9ff45b0c18cb58f2
timpituch <tpituch@umich.edu>
2014-11-21 14:54:38 -0500

    Added email contact processing php file

--- contact.php
+++ contact.php
@@ -3,112 +3,114 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Los Graduados - Contact</title>
-	<link rel="stylesheet" type="text/css" href="style.css"/>
+	<link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+	<link href="css/style.css" rel="stylesheet" type="text/css">
 	<meta name ="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
 	<?php include("header.php"); ?> 
-	<h2>Want to chat? Shoot me a message!</h2>
+	<h2>Want to chat? Shoot us a message!</h2>
 	<div class="form">
-	<form name="contactform" method="post" action="send_form_email.php">
+	<form name="contactform" method="post" action="contact_email.php">
  
-<table>
+	<table>
  
-<tr>
+		<tr>
  
- <td valign="top">
+ 			<td valign="top">
  
-  <label for="first_name">First Name *</label>
+  				<label for="first_name">First Name *</label>
  
- </td>
+ 			</td>
  
- <td valign="top">
+ 			<td valign="top">
  
-  <input  type="text" name="first_name" maxlength="50" size="30">
+  				<input  type="text" name="first_name" maxlength="50" size="30">
  
- </td>
- 
-</tr>
- 
-<tr>
- 
- <td valign="top">
- 
-  <label for="last_name">Last Name *</label>
- 
- </td>
- 
- <td valign="top">
- 
-  <input  type="text" name="last_name" maxlength="50" size="30">
- 
- </td>
- 
-</tr>
- 
-<tr>
- 
- <td valign="top">
- 
-  <label for="email">Email Address *</label>
- 
- </td>
- 
- <td valign="top">
- 
-  <input  type="text" name="email" maxlength="80" size="30">
- 
- </td>
- 
-</tr>
- 
-<tr>
- 
- <td valign="top">
- 
-  <label for="telephone">Telephone Number</label>
- 
- </td>
- 
- <td valign="top">
- 
-  <input  type="text" name="telephone" maxlength="30" size="30">
- 
- </td>
- 
-</tr>
- 
-<tr>
- 
- <td valign="top">
- 
-  <label for="comments">Comments *</label>
- 
- </td>
- 
- <td valign="top">
- 
-  <textarea  name="comments" maxlength="1000" cols="25" rows="6"></textarea>
- 
- </td>
- 
-</tr>
- 
-<tr>
- 
- 		<td colspan="2" style="text-align:center">
- 
-  		<input type="submit" value="Submit">   
- 
- 		</td>
+ 			</td>
  
 		</tr>
  
-		</table>
+		<tr>
  
-		</form>
+ 			<td valign="top">
+ 
+  				<label for="last_name">Last Name *</label>
+ 
+ 			</td>
+ 
+ 			<td valign="top">
+ 
+ 				 <input  type="text" name="last_name" maxlength="50" size="30">
+ 
+ 			</td>
+ 
+		</tr>
+ 
+		<tr>
+ 
+ 			<td valign="top">
+ 
+ 				<label for="email">Email Address *</label>
+ 
+ 			</td>
+ 
+ 			<td valign="top">
+ 
+  				<input  type="text" name="email" maxlength="80" size="30">
+ 
+ 			</td>
+ 
+		</tr>
+ 
+		<tr>
+ 
+ 			<td valign="top">
+ 
+  				<label for="telephone">Telephone Number</label>
+ 
+ 			</td>
+ 
+ 			<td valign="top">
+ 
+  				<input  type="text" name="telephone" maxlength="30" size="30">
+ 
+ 			</td>
+ 
+		</tr>
+ 
+		<tr>
+ 
+ 			<td valign="top">
+ 
+  				<label for="comments">Comments *</label>
+ 	
+ 			</td>
+ 
+ 			<td valign="top">
+ 
+  				<textarea  name="comments" maxlength="1000" cols="25" rows="6"></textarea>
+ 
+ 			</td>
+ 
+		</tr>
+ 
+		<tr>
+ 
+ 			<td colspan="2" style="text-align:center">
+ 
+  				<input type="submit" value="Submit">   
+ 
+ 			</td>
+ 
+		</tr>
+ 
+	</table>
+ 
+	</form>
 	</div>
-	<?php include("footer.php"); ?>
+
+<?php include("footer.php"); ?>
 
 </body>
 </html>
