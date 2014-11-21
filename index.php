8944a5acc7e91597a200f485f3ea56aca647b382
timpituch <tpituch@umich.edu>
2014-11-21 14:17:12 -0500

    Getting it started!
    Trying to figure out how this github thing works. I just added the
    index.php file to the folder.
    
    Tim

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Los Graduados - Latino Graduate Student Organization at the University of Michigan</title>
+</head>
+<body>
+	
+</body>
+</html>
