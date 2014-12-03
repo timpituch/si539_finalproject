3a9f22520ee263d7b77ba310c57a731b0f01892f
mandigonzalez <gonzalam@umich.edu>
2014-12-02 20:08:11 -0500

    Added welcoming content for index page

--- index.php
+++ index.php
@@ -10,8 +10,20 @@
 <body>
 	<?php $fname = basename(__FILE__); ?> 
 	<?php include("header.php"); ?>
-	
-	<h1> Welcome! </h1>
+
+<div class="container">
+	<div class="row">
+		<div class="col-md-12">
+			<h1> Welcome/Bienvenidos/Bem-vindos! </h1>
+			<p>
+			Los Graduados is a new pan-Latino cultural group for graduate students at the University of Michigan.  The organization provides the opportunity for graduate students to connect with their peers in other graduate programs over food, drinks, conversation and action.  Board members and special guests facilitate discussion on topics important to the Latino community and encourage personal and professional development through peer to peer education.
+
+			The first official meeting of Los Graduados will be January XX, 2014 at X:XXpm at XXX.  Please save the date to your calendar!  If you would like to share this event with your graduate community, please feel free to do so. 
+			Click here to join our mailing list.
+			</p>
+		</div>
+	</div>
+</div>
 
 	<?php include("footer.php"); ?>
 </body>
