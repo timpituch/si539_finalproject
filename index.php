6f5b75dd24cb0a3c5332929f9106ec026c895818
mandigonzalez <gonzalam@umich.edu>
2014-12-04 09:41:41 -0500

    updated index

--- index.php
+++ index.php
@@ -23,16 +23,26 @@
 	<div class="container">
 	<div class="row">
 		<div class="col-md-12">
-			<h1> Welcome/Bienvenidos/Bem-vindos! </h1>
-			<p>
-			Los Graduados is a new pan-Latino cultural group for graduate students at the University of Michigan.  The organization provides the opportunity for graduate students to connect with their peers in other graduate programs over food, drinks, conversation and action.  Board members and special guests facilitate discussion on topics important to the Latino community and encourage personal and professional development through peer to peer education.
-
-			The first official meeting of Los Graduados will be January XX, 2014 at X:XXpm at XXX.  Please save the date to your calendar!  If you would like to share this event with your graduate community, please feel free to do so. 
-			Click here to join our mailing list.
-			</p>
+			<div class="page-header" style="text-align:center">
+				<h1> Welcome/Bienvenidos/Bem-vindos! </h1>
+			</div>
+			
+			<div class="row">
+				<div class="col-md-6">
+					<img src="img/latinos.jpg" alt="latinos" class="img-circle" width="100%">
+				</div>
+				<div class="col-md-6">
+					<p>
+					Los Graduados is a new pan-Latino cultural group for graduate students at the University of Michigan.  The organization provides the opportunity for graduate students to connect with their peers in other graduate programs over food, drinks, conversation and action.  Board members and special guests facilitate discussion on topics important to the Latino community and encourage personal and professional development through peer to peer education.
+					The first official meeting of Los Graduados will be January XX, 2014 at X:XXpm at XXX.  Please save the date to your calendar!  If you would like to share this event with your graduate community, please feel free to do so. 
+					Click here to join our mailing list.
+					</p>
+				</div>
+			</div>
 		</div>
 	</div>
 	</div>
+	<hr class="featurette-divider">
  <!--FOOTER -->
 	<?php include("footer.php"); ?>
 </body>
